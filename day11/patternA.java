60164908f3fa61de8cb0f1e0586eaf14fe22ba12
aravindpandiyarajan2004 <83591853+aravindpandiyarajan2004@users.noreply.github.com>
2021-09-11 21:24:26 +0530

    Add files via upload

---
+++ day11/patternA.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class patternA
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+pattern a=new pattern();
+a.display(7);
+}
+}
+class pattern
+{
+void display(int n)
+{
+for(int i=0;i<=n;i++)
+{
+for(int j=0;j<=n/2;j++)
+{
+if((j==0 || j==n/2) && i!=0 || i==0 && j!=n/2 || i==n/2)
+System.out.print("*");
+else
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
